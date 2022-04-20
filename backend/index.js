7c74d2bfb275a0c8a3592c7626d90cba545a267b
Andii Shapoval <andrii.shapoval@smile-ukraine.com>
2022-04-20 18:07:31 +0300

    added pin

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const pinRoute = require('./routes/pins');
 
 const app = express();
 
 dotenv.config();
 
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, 
     {
@@ -17,6 +20,8 @@ mongoose.connect(process.env.MONGO_URL,
 
 
 app.post("users/register/");
+
+app.use('/api/pins', pinRoute);
 
 app.listen(8800, () => {
     console.log('Backend server is running eder');
