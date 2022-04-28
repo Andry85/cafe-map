dc7eba6eb4261347775f8f84a06fa832da71a6b0
Andii Shapoval <andrii.shapoval@smile-ukraine.com>
2022-04-28 16:13:43 +0300

    added popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import * as React from 'react';
-import Map, {Marker, Popup} from 'react-map-gl';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import StarIcon from '@mui/icons-material/Star';
+import CloseIcon from '@mui/icons-material/Close';
 import './app.css';
+import Map, {Marker, Popup} from 'react-map-gl';
 import axios from 'axios';
+
 
 
 function App() {
 
   const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [ispopupVisible, setIspopupVisible] = useState(true);
+
 
 
   useEffect(() => {
@@ -27,6 +32,17 @@ function App() {
     console.log(pins);
   }, [])
 
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+    setIspopupVisible(true)
+  }
+
+  const handleClose = () => {
+    setIspopupVisible(false);
+  }
+
+
+
   return (
     <div className="App">
       <Map
@@ -39,29 +55,47 @@ function App() {
         style={{width: '100vw', height: '100vh'}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        {pins.map(p=>(
+        {pins.map((p) =>(
           <>
             <Marker longitude={p.long} latitude={p.lat} anchor="bottom" >
-              <LocationOnIcon sx={{ fontSize: 60, color: "slateblue" }}  />
+              <LocationOnIcon 
+                sx={{ fontSize: 60, color: "slateblue" }} 
+                onClick={()=>handleMarkerClick(p._id)}
+               />
+              {p._id === currentPlaceId && ispopupVisible &&  (
+                <>
+                  <div className="popup">
+                    <CloseIcon className="popup-close" onClick={handleClose} />
+                    <div className="card">
+                      <label>Place</label>
+                      <h4 className="place">{p.title}</h4>
+                      <label>Review</label>
+                      <p className="desc">{p.description}</p>
+                      <label>Rating</label>
+                      <div className="stars">
+                        <StarIcon className="star" />
+                        <StarIcon className="star"/>
+                        <StarIcon className="star"/>
+                        <StarIcon className="star"/>
+                      </div>
+                      <label>Information</label>
+                      <span className="username">Created by <b>{p.username}</b></span>
+                      <span className="date">{p.createdAt}</span>
+                    </div>
+                  </div>
+                </>
+              )
+            }
+
             </Marker>
-            <Popup longitude={p.long} latitude={p.lat} anchor="left">
-              <div className="card">
-                <label>Place</label>
-                <h4 className="place">{p.title}</h4>
-                <label>Review</label>
-                <p className="desc">{p.description}</p>
-                <label>Rating</label>
-                <div className="stars">
-                  <StarIcon className="star" />
-                  <StarIcon className="star"/>
-                  <StarIcon className="star"/>
-                  <StarIcon className="star"/>
-                </div>
-                <label>Information</label>
-                <span className="username">Created by <b>{p.username}</b></span>
-                <span className="date">1 hour ago</span>
-              </div>
-            </Popup>
+
+            
+
+            
+            
+            
+
+            
             </>
         ))}
 
